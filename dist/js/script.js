76aa93d81566101c9d5f72c92927fb4b4b27aa41
Tamara <tamara.herashchenko@gmail.com>
2022-01-08 16:15:50 +0200

    Update modal script

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,28 +6,54 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Modal
     const modalBtn = document.querySelector('[data-modal]'),
-          modal = document.querySelector('.modal'),
-          modalClose = document.querySelector('.modal__close');
+          modalBlock = document.querySelector('.modal'),
+          closeBtn = document.querySelector('.close');
+
+    const sections = document.querySelectorAll('body > *');
     
-    function openModal() {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
+    function openModal(block) {
+        block.classList.add('show');
+        block.classList.remove('hide');
         document.body.style.overflow = 'hidden';
+
+        sections.forEach(item => {
+            if (item.classList.contains('sidepanel')) {
+                    
+            } else {
+                item.style.paddingRight = '17px';
+            }
+        });
     }
 
-    function closeModal() {
-        modal.classList.add('hide');
-        modal.classList.remove('show');
+    function closeModal(block) {
+        block.classList.add('hide');
+        block.classList.remove('show');
         document.body.style.overflow = '';
+
+        sections.forEach(item => {
+            if (item.classList.contains('sidepanel')) {
+                    
+            } else {
+                item.style.paddingRight = '0px';
+            }
+        });
     }
 
-    modalBtn.addEventListener('click', openModal);
-    modalClose.addEventListener('click', closeModal);
-
-    modal.addEventListener('click', (event) => {
-        if (event.target == modal) {
-            closeModal();
-        }
+    modalBtn.addEventListener('click', () => {
+        openModal(modalBlock);
     });
+
+    closeBtn.addEventListener('click', () => {
+        closeModal(modalBlock);
+    }); 
+
+    function closeModalAside(block) {
+        block.addEventListener('click', (event) => {
+            if (event.target == block) {
+                closeModal(block);
+            }
+        });
+    }
+    closeModalAside(modalBlock);
 
 });
